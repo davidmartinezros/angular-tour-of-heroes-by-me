6507abc93a7400328fb0da78c55a1a17f2ae5443
David <david.martinez.etic@gmail.com>
2016-10-17 11:44:32 +0200

    change the html template making an input for changing the name of the hero

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,7 +5,10 @@ import { Component } from '@angular/core';
     <h1>{{title}}</h1>
     <h2>{{hero.name}} details!</h2>
     <div><label>id: </label>{{hero.id}}</div>
-    <div><label>name: </label>{{hero.name}}</div>
+    <div>
+      <label>name: </label>
+      <input value="{{hero.name}}" placeholder="name">
+    </div>
   `
 })
 export class AppComponent {
