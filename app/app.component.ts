543eb5960c87a71e1df7d48be64d39ace1ca9e32
David <david.martinez.etic@gmail.com>
2016-10-17 11:34:19 +0200

    begin the develop: obtains the data template from a typescript class

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  template: '<h1>{{title}}</h1><h2>{{hero}} details!</h2>'
+})
+export class AppComponent {
+  title = 'Tour of Heroes';
+  hero = 'Windstorm';
+}
