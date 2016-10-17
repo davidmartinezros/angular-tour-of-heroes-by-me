2f1f94f7d8860afa0cdb34a1ca1265502703eb65
David <david.martinez.etic@gmail.com>
2016-10-17 12:51:13 +0200

    Show the selected Heroe only if is seleted and save the info detail of a heroe with the event onSelect of the AppComponent class

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,16 +3,18 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
-    <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="hero.name" placeholder="name">
+    <div *ngIf="selectedHero">
+      <h2>{{selectedHero.name}} details!</h2>
+      <div><label>id: </label>{{selectedHero.id}}</div>
+      <div>
+        <label>name: </label>
+        <input [(ngModel)]="selectedHero.name" placeholder="name">
+      </div>
     </div>
 
     <h2>My Heroes</h2>
     <ul class="heroes">
-      <li *ngFor="let hero of heroes">
+      <li *ngFor="let hero of heroes" (click)="onSelect(hero)">
          <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
@@ -74,6 +76,11 @@ export class AppComponent {
     name: 'Windstorm'
   }
   heroes = HEROES;
+  selectedHero: Hero;
+  
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  };
 }
 
 export class Hero {
