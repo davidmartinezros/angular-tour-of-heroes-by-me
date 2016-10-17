2093560b289a7c974d44bd499508eef79324530f
David <david.martinez.etic@gmail.com>
2016-10-17 15:37:44 +0200

    Export mock-heroes to a file like a mock data source

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 
 import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
 
 @Component({
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
-    
+
     <my-hero-detail [hero]="selectedHero"></my-hero-detail>
 
     <h2>My Heroes</h2>
@@ -77,16 +78,3 @@ export class AppComponent {
     this.selectedHero = hero;
   };
 }
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
