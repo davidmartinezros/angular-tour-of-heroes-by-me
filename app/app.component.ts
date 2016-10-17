1f84b1a0550e7dc0a71594c4b9f0bc12d95c19f3
David <david.martinez.etic@gmail.com>
2016-10-17 12:57:00 +0200

    Add the binding class.selected in the li array list of heroes for set a style class when the heroe is seleted

--- app/app.component.ts
+++ app/app.component.ts
@@ -14,7 +14,9 @@ import { Component } from '@angular/core';
 
     <h2>My Heroes</h2>
     <ul class="heroes">
-      <li *ngFor="let hero of heroes" (click)="onSelect(hero)">
+      <li *ngFor="let hero of heroes"
+        [class.selected]="hero === selectedHero" 
+        (click)="onSelect(hero)">
          <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
